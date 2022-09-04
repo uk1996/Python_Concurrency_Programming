081a8f514d4681de4e16d21fcf074b1f5195e7a1
uk1996 <csuk1947@gmail.com>
2022-09-04 22:45:05 +0900

    update 02-1_sync.py

---
+++ 1. 파이썬 코루틴과 비동기 함수/02-1_sync.py
@@ -0,0 +1,22 @@
+# 코드가 동기적으로 동작한다 => 코드가 반드시 작성된 순서 그대로 실행된다.
+import time
+
+
+def delivery(name, mealtime):
+    print(f"{name}에게 배달 완료!")
+    time.sleep(mealtime)
+    print(f"{name} 식사 완료, {mealtime}시간 소요...")
+    print(f"{name} 그릇 수거 완료")
+
+
+def main():
+    delivery("A", 3)
+    delivery("B", 3)
+    delivery("C", 4)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    end = time.time()
+    print(end - start)
