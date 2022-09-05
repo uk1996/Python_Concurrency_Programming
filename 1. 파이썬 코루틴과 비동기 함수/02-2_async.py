fc51d8b8a97b6985c95b64831fb56aed50ea4e77
uk1996 <csuk1947@gmail.com>
2022-09-05 22:30:24 +0900

    update 02-2_async.py

--- 1. 파이썬 코루틴과 비동기 함수/02-2_async.py
+++ 1. 파이썬 코루틴과 비동기 함수/02-2_async.py
@@ -3,11 +3,12 @@ import time
 import asyncio
 
 # 코루틴 함수
-async def delivery(name, mealtime):
+async def delivery(name, mealtime):  # 진입점 1
     print(f"{name}에게 배달 완료!")
-    await asyncio.sleep(mealtime)
+    await asyncio.sleep(mealtime)  # 탈출점 1 / 진입점 2
     print(f"{name} 식사 완료, {mealtime}시간 소요...")
     print(f"{name} 그릇 수거 완료")
+    # 탈출점 2
 
 
 async def main():
